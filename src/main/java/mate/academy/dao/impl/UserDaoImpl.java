71c14e74a7f430ec390978678f2c0c8eb8fde9eb
filipchelkowski <filip.chelkowski@gmail.com>
2026-05-24 12:14:51 +0200

    Update UserDaoImpl.java

--- src/main/java/mate/academy/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -2,7 +2,11 @@ package mate.academy.dao.impl;
 
 import mate.academy.dao.UserDao;
 import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.Optional;
 
@@ -10,11 +14,39 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User add(User user) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add user " + user + " to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return user;
     }
 
     @Override
     public Optional<User> findByEmail(String email) {
-        return Optional.empty();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        try (Session session = sessionFactory.openSession()) {
+            Query<User> query = session.createQuery(
+                    "FROM User WHERE email = :email", User.class);
+            query.setParameter("email", email);
+            return query.uniqueResultOptional();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find user by email: " + email, e);
+        }
     }
 }
