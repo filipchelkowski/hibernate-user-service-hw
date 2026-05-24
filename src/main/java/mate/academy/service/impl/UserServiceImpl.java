b78f1208f3647087baac520332dfdaf742141971
filipchelkowski <filip.chelkowski@gmail.com>
2026-05-24 12:14:54 +0200

    Create UserServiceImpl.java

---
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package mate.academy.service.impl;
+
+import mate.academy.dao.UserDao;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+
+    private UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public User add(User user) {
+        user.setSalt(HashUtil.getSalt());
+        user.setPassword(HashUtil.hashPassword(user.getPassword(), user.getSalt()));
+        return userDao.add(user);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+}
