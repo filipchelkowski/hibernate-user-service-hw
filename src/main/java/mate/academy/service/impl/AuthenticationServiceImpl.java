7508df283e7941cc2ac023cd8abd6f14e35af75e
filipchelkowski <filip.chelkowski@gmail.com>
2026-05-24 12:30:51 +0200

    Create AuthenticationServiceImpl.java

---
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,51 @@
+package mate.academy.service.impl;
+
+import mate.academy.exception.AuthenticationException;
+import mate.academy.exception.RegistrationException;
+import mate.academy.model.User;
+import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.HashUtil;
+
+import java.util.Optional;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private UserService userService;
+
+    public AuthenticationServiceImpl(UserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFind = userService.findByEmail(email);
+
+        if (userFind.isEmpty()) {
+            throw new AuthenticationException("User with email: " + email + " doesn't exist");
+        }
+
+        User user = userFind.get();
+
+        String hashPassword = HashUtil.hashPassword(password, user.getSalt());
+
+        if (hashPassword.equals(user.getPassword())) {
+            return user;
+        } else {
+            throw new AuthenticationException("Password is incorrect!");
+        }
+    }
+
+    @Override
+    public User register(String email, String password) throws RegistrationException {
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("User with email: " + email + " already exists");
+        }
+
+        User user = new User();
+        user.setEmail(email);
+        user.setSalt(HashUtil.getSalt());
+        user.setPassword(HashUtil.hashPassword(password, user.getSalt()));
+
+        return userService.add(user);
+    }
+}
