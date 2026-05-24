586d87501463a84b84b5f503509c9bc7435191fc
filipchelkowski <filip.chelkowski@gmail.com>
2026-05-24 11:49:03 +0200

    Create UserDaoImpl.java

---
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -0,0 +1,20 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.UserDao;
+import mate.academy.model.User;
+import org.hibernate.SessionFactory;
+
+import java.util.Optional;
+
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public User add(User user) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.empty();
+    }
+}
